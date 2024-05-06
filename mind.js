f645cbfbe51552175311d9668cbb7eb3f043d2d0
Yasusus <129669758+Yasusus@users.noreply.github.com>
2024-05-07 02:14:25 +0800

    Update mind.js

--- mind.js
+++ mind.js
@@ -12,9 +12,9 @@ let secondCard = false;
 
 //Items array
 const items = [
-  { name: "bee", image: "bee.png" },
-  { name: "crocodile", image: "crocodile.png" },
-  { name: "macaw", image: "macaw.png" },
+  { id: 1, text: "Item 1" },
+  { id: 2, text: "Item 2" },
+  { id: 3, text: "Item 3" },
   { name: "gorilla", image: "gorilla.png" },
   { name: "tiger", image: "tiger.png" },
   { name: "monkey", image: "monkey.png" },
